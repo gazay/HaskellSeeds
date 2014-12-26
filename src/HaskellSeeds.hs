768de1f871e29ecf51fdd358c3eccd25f8dacdd3
Alexey Gaziev <alex.gaziev@gmail.com>
2014-12-26 18:55:05 +0800

    Help message function

--- src/HaskellSeeds.hs
+++ src/HaskellSeeds.hs
@@ -8,6 +8,16 @@ import System.Environment
 main :: IO ()
 main = getArgs >>= runTask
 
+helpMessage :: String
+helpMessage = unlines [ "Help message:"
+                      , ""
+                      , " build - builds your library or executable with cabal install."
+                      , "         By default runs hlint and builds documentation with haddock"
+                      , "         for uploading to hackage"
+                      , " push  - uploads your library or executable to hackage. Uploads documentation by default"
+                      , " help  - shows this message"
+                      ]
+
 runTask :: [String] -> IO ()
 runTask ("help":args)   = print helpMessage
 runTask ("--help":args) = print helpMessage
