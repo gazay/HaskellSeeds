a16ee4b1bf431bed6e2b70f6a0015704ca01d3c3
Alexey Gaziev <alex.gaziev@gmail.com>
2014-12-26 19:02:51 +0800

    Better help message and buildable code

--- src/HaskellSeeds.hs
+++ src/HaskellSeeds.hs
@@ -9,7 +9,11 @@ main :: IO ()
 main = getArgs >>= runTask
 
 helpMessage :: String
-helpMessage = unlines [ "Help message:"
+helpMessage = unlines [ "HaskellSeeds"
+                      , " version: 0.1.0.0"
+                      , " repo:    https://github.com/gazay/HaskellSeeds"
+                      , ""
+                      , "Help message:"
                       , ""
                       , " build - builds your library or executable with cabal install."
                       , "         By default runs hlint and builds documentation with haddock"
@@ -19,15 +23,19 @@ helpMessage = unlines [ "Help message:"
                       ]
 
 runTask :: [String] -> IO ()
-runTask ("help":args)   = print helpMessage
-runTask ("--help":args) = print helpMessage
-runTask ("-h":args)     = print helpMessage
+runTask ("help":args)   = putStrLn helpMessage
+runTask ("--help":args) = putStrLn helpMessage
+runTask ("-h":args)     = putStrLn helpMessage
 runTask ("build":args)  = do
                                 result <- buildSeed args
-                                print result
+                                putStrLn result
 runTask ("push":args)   = do
                                 result <- pushSeedWithDocs args
-                                print result
-runTask _               = print "Command is not supported"
+                                putStrLn result
+runTask _               = putStrLn "Command is not supported"
 
+buildSeed :: [String] -> IO String
+buildSeed = undefined
 
+pushSeedWithDocs :: [String] -> IO String
+pushSeedWithDocs = undefined
