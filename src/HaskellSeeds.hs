136dc04d75be9c7daf27ce4f96e5e14fbdf66e96
Alexey Gaziev <alex.gaziev@gmail.com>
2014-12-28 22:51:28 +0800

    Prototype for build function

--- src/HaskellSeeds.hs
+++ src/HaskellSeeds.hs
@@ -3,6 +3,7 @@
 -- MIT License
 --
 import System.Environment
+import System.Process
 
 -- | 'main' runs the main program
 main :: IO ()
@@ -34,8 +35,31 @@ runTask ("push":args)   = do
                                 putStrLn result
 runTask _               = putStrLn "Command is not supported"
 
+haddockArgs :: String -> [String]
+haddockArgs libName = [ "haddock"
+                      , "--hyperlink-source"
+                      , "--html-location='http://hackage.haskell.org/package/" ++ libName ++ "/docs'"
+                      , "--contents-location='http://hackage.haskell.org/package/" ++ libName ++ "'"
+                      ]
+
 buildSeed :: [String] -> IO String
-buildSeed = undefined
+buildSeed = do
+    putStrLn "Looking for cabal file..."
+    cabalFile <- readProcess "ls" ["*.cabal"] ""
+    putStrLn cabalFile
+    putStrLn "Found " ++ cabalFile
+    putStrLn "Creating Sandbox..."
+    result1 <- readProcess "cabal" ["sandbox", "init"] ""
+    putStrLn result1
+    putStrLn "Sandbox created"
+    putStrLn "Installing seed into sandbox..."
+    result2 <- readProcess "cabal" ["install", "-j"] ""
+    putStrLn result2
+    putStrLn "Installation completed"
+    putStrLn "Generating docs for seed..."
+    result3 <- readProcess "cabal" haddockArgs(cabalFile) ""
+    return result3
+
 
 pushSeedWithDocs :: [String] -> IO String
 pushSeedWithDocs = undefined
